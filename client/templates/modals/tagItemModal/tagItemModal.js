5d550c5b397dc5aa77d2051ce115b7fa3d2c3abb
Joseph Pollack <jpollack320@gmail.com>
2016-10-29 16:52:59 -0400

    improved tagging

--- client/templates/modals/tagItemModal/tagItemModal.js
+++ client/templates/modals/tagItemModal/tagItemModal.js
@@ -7,7 +7,6 @@ Template.tagItemModal.helpers({
 Template.tagItemModal.events({
 	'click .ok-button' : function(element) {
 		var item_id = $(element.target).data().item_id;
-
 		var tag_array = commaSeparatedValuesToArray($('#tag-entry').val());
 
 		Meteor.call('tagItem', item_id, tag_array, function(error) {
@@ -16,5 +15,22 @@ Template.tagItemModal.events({
 		});
 
 		$('.template-modalTemplate').remove();
-	}
+	},
+
+	'keydown #tag-entry': function(event) {
+		console.log(event);
+		if (event.key == "Enter") {
+			event.preventDefault();
+
+			var item_id = $(event.target).data().item_id;
+			var tag_array = commaSeparatedValuesToArray($('#tag-entry').val());
+
+			Meteor.call('tagItem', item_id, tag_array, function(error) {
+				if (error)
+					console.log(error.message);
+			});
+
+			$('.template-modalTemplate').remove();
+		}
+	}, 
 })
