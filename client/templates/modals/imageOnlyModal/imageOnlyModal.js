d7d13a2a849616efd4474c9530605bbbebfb49f4
Joseph Pollack <jpollack320@gmail.com>
2016-11-11 17:26:49 -0500

    checklist beta finished

--- client/templates/modals/imageOnlyModal/imageOnlyModal.js
+++ client/templates/modals/imageOnlyModal/imageOnlyModal.js
@@ -1,19 +1,5 @@
-var full_size_tracker = new Tracker.Dependency;
-var full_container_width;
-var full_container_height;
-
 Template.imageOnlyModal.helpers({
 	'getRarity' : function(artwork_id) {
 		return artworks.findOne(artwork_id).rarity;
 	}
 })
-
-Template.imageOnlyModal.rendered = function() {
-	if ($('.img-container').length != 0) {
-		full_container_height = $('.img-container').height();
-		full_container_width = $('.img-container').width(); 
-		full_size_tracker.changed();
-	}
-
-	sought_status = {};
-}
