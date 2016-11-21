4b4fbc4365908cb22a8123e13433a1e4e28c91a9
Joseph Pollack <jpollack320@gmail.com>
2016-11-20 21:22:15 -0500

    fix quick sell auction typo

--- server/item_methods.js
+++ server/item_methods.js
@@ -349,7 +349,7 @@ Meteor.methods({
                 else {
                     if (Meteor.user().profile.user_type != "admin") {
                         if (Math.random() < 0.5) {
-                            createAuction(item_id, getItemObjectValueByType(item_object, "sell", Meteor.userId()), -1, 120, "public");
+                            createAuction(item_id, getItemObjectValueByType(item_object, "actual", Meteor.userId()), -1, 120, "public");
                         }
 
                         else {
