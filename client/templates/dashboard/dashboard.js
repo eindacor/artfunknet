e53661a2a5a43426d4245c749c2b927908df627e
Joseph Pollack <jpollack320@gmail.com>
2016-11-28 14:15:56 -0500

    gut old dashboard code

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -1,6 +1,5 @@
 var current_section_tracker = new Tracker.Dependency;
 var current_section = "profile";
-var current_user_id = Meteor.userId();
 
 Template.dashboard.helpers({
 	'screen_name': function() {
@@ -21,29 +20,7 @@ Template.dashboard.helpers({
 	}
 })
 
-Template.dashboard.events({
-	'change .price-selector' : function() {
-		var entry_fee = $('.price-selector').val();
-		Meteor.call('updateEntryFee', entry_fee, function(error) {
-			if (error)
-				console.log(error.message);
-		});
-	}
-});
-
 Template.dashboard.rendered = function() {
-	$('#entry-fee').slider({
-		'value': Meteor.user().profile.entry_fee,
-		'max': 100000,
-		'min': 0,
-		'change': function(event, ui) {
-			Meteor.call('updateEntryFee', ui.value - (ui.value % 1000), function(error) {
-				if (error)
-					console.log(error.message)
-			});
-		}
-	});
-
 	if (Meteor.user().profile.tutorials.welcome) {
 		Blaze.renderWithData(Template.modalTemplate, {
 			'modal_name': "tutorialModal", 
@@ -119,126 +96,3 @@ Template.dashboard.rendered = function() {
 		}, $('body')[0]);
 	}
 }
-
-Template.info.helpers({
-	'xpData' : function() {
-		Meteor.call('getXPData', Meteor.user().profile.level, function(error, result) {
-			if (error)
-				console.log(error.message);
-
-			else {
-				var xp_data = result;
-				var completion = (Meteor.user().profile.xp / xp_data.goal) * 100;
-				var xp_object = {
-					'xp_completion' : Math.floor(completion) > 100 ? 100 : Math.floor(completion),
-					'xp_remaining' : getCommaSeparatedValue(xp_data.goal - Meteor.user().profile.xp),
-					'xp_goal' : getCommaSeparatedValue(xp_data.goal)
-				};
-
-				Session.set('xp_data', xp_object);
-			}
-		});
-
-		if (Session.get('xp_data'))
-			return Session.get('xp_data');
-
-		else return {
-			'xp_completion' : 0,
-			'xp_remaining' : 0,
-			'xp_goal' : 0
-		}
-	},
-
-	'user_object': function() {
-		return Meteor.user()
-	},
-
-	'display_count': function() {
-		return items.find({'owner' : current_user_id, 'status' : 'displayed'}).count();
-	},
-
-	'inventory_count': function() {
-		return items.find({'owner' : current_user_id, 'status' : {$nin : ['unclaimed', 'for_sale', 'won']}}).count();
-	},
-
-	'slots_available': function() {
-		return Meteor.user().profile.inventory_cap + Meteor.user().profile.expansion_slots - items.find({'owner' : Meteor.userId(), 'status' : {$nin : ['unclaimed', 'for_sale', 'won']}, 'original': {$ne: true}, 'vintage': {$ne: true}}).count();
-	},
-
-	'original_count': function() {
-		return items.find({'owner' : current_user_id, 'status' : {$nin : ['unclaimed', 'for_sale', 'won']}, 'original': true}).count();
-	},
-
-	'vintage_count': function() {
-		return items.find({'owner' : current_user_id, 'status' : {$nin : ['unclaimed', 'for_sale', 'won']}, 'vintage': true}).count();
-	},
-
-	'permanent_count': function() {
-		return items.find({'owner' : current_user_id, 'status' : 'permanent'}).count();
-	},
-
-	'max_total': function() {
-		return Meteor.user().profile.inventory_cap + Meteor.user().profile.expansion_slots;
-	},
-
-	'auctioned_items': function() {
-		auctions.find({'seller': Meteor.user().profile.screen_name}).count();
-	},
-
-	'auction_cap': function() {
-		var has_auctioneer = Meteor.user().profile.market_expert.expiration > moment()._d.toISOString();
-		Math.floor(Meteor.user().profile.auction_cap * (has_auctioneer ? 1.5 : 1))
-	},
-
-	'collection_value' : function() {
-		Meteor.call('getCollectionValue', Meteor.userId(), function(error, result) {
-			if (error)
-				console.log(error.message);
-
-			else Session.set('collection_value', result);
-		});
-
-		if (Session.get('collection_value') !== undefined)
-			return getCommaSeparatedValue(Session.get('collection_value'));
-
-		else return "";
-	},
-
-	'display_value' : function() {
-		Meteor.call('getExhibitionValue', Meteor.userId(), function(error, result) {
-			if (error)
-				console.log(error.message);
-
-			else Session.set('display_value', result);
-		});
-
-		if (Session.get('display_value') !== undefined)
-			return getCommaSeparatedValue(Session.get('display_value'));
-
-		else return "";
-	},
-
-	'ticket' : function() {
-		return gallery_tickets.find({'ticketholder': Meteor.userId()});
-	},
-
-	'max_level': function() {
-		return Meteor.user().profile.level >= 50;
-	},
-
-	'unselected': function(current_tier) {
-		var tier_array = ["free", "low", "medium", "high", "outrageous"];
-		tier_array.splice(tier_array.indexOf(current_tier), 1);
-		return tier_array;
-	},
-
-})
-
-Template.info.events({
-	'click #vintage-mode': function() {
-		Blaze.renderWithData(Template.modalTemplate, {
-			'modal_name': "vintageModal", 
-			'modal_data': undefined
-		}, $('body')[0]);
-	}
-})
