b6fa7b6d122a9177eee1c3db4e6d7ab672c75df2
Joe Pollack <jpollack320@gmail.com>
2016-12-16 05:27:07 +0000

    update att color

--- client/global_helpers.js
+++ client/global_helpers.js
@@ -9,7 +9,7 @@ Template.registerHelper('consoleLogThis', function(the_thing) {
 
 Template.registerHelper('getHTMLColorFromValue', function(value) {
 	if (value === undefined)
-		return "rgb(180, 180, 180)";
+		return "rgb(150, 150, 150)";
 
 	var red_value = 255 - Math.floor(value * 255);
 	var color_string = "rgb(" + red_value + " , 0, 0)";
