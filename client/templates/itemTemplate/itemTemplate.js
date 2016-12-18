a4a74415a3d9a13ebf57c2bfea638be5bebb8007
Joseph Pollack <jpollack320@gmail.com>
2016-12-18 01:18:12 -0500

    separate item actions from item template

--- client/templates/itemTemplate/itemTemplate.js
+++ client/templates/itemTemplate/itemTemplate.js
@@ -151,6 +151,10 @@ Template.itemInfo.helpers({
 
 	'showDetails': function(item_data) {
 		return item_data.xp_rating != undefined;
+	},
+
+	'action_button': function(item_data) {
+
 	}
 })
 
@@ -165,36 +169,6 @@ Template.itemInfo.events({
 				'modal_data': {
 					'item_data': items.findOne(item_id)
 				}
-			}, $('body')[0]);
-		}
-	},
-
-	'click .quick-sell.enabled' : function(element) {
-		element.stopPropagation();
-		var item_id = $(element.target).closest('.card-container').data('item_id');
-		var item_object = items.findOne(item_id);
-
-		var user_object = Meteor.user();
-		//TODO add sought filter
-		if (
-			user_object.profile.settings.quick_sell_options.standard &&
-   			item_object.artwork_data.rarity != "masterpiece" &&
-   			!item_object.seasonal &&
-   			item_object.lottery == 0 &&
-			(!item_object.foil || user_object.profile.settings.quick_sell_options.foil) &&
-			(item_object.artwork_data.rarity != "legendary" || user_object.profile.settings.quick_sell_options.legendary) &&
-			(quests.findOne({'owner_id': Meteor.userId(), 'target': {$in: [item_object.artwork_id]}}) == undefined || user_object.profile.settings.quick_sell_options.quest_items)
-			) {
-			Meteor.call('sellArtwork', item_id, function(error) {
-				if (error)
-					console.log(error.message);
-			});
-		}
-
-		else {
-			Blaze.renderWithData(Template.modalTemplate, {
-				'modal_name': "quickSellModal", 
-				'modal_data': items.findOne(item_id)
 			}, $('body')[0]);
 		}
 	},
