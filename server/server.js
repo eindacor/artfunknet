f9fc290a469d6605321c9964a684ce4afe46a880
Joseph Pollack <jpollack320@gmail.com>
2017-04-30 01:13:24 -0400

    prevent unowned type quick sell

--- server/server.js
+++ server/server.js
@@ -46,6 +46,8 @@ var updateContent = function() {
     Meteor.users.update({'profile.settings.quick_sell_options.unlocked': null}, {$set: {'profile.settings.quick_sell_options.unlocked': false}}, {multi: true});
     Meteor.users.update({'profile.settings.quick_sell_options.original': null}, {$set: {'profile.settings.quick_sell_options.original': false}}, {multi: true});
     Meteor.users.update({'profile.settings.quick_sell_options.lottery': null}, {$set: {'profile.settings.quick_sell_options.lottery': false}}, {multi: true});
+    Meteor.users.update({'profile.settings.quick_sell_options.vintage': null}, {$set: {'profile.settings.quick_sell_options.vintage': false}}, {multi: true});
+    Meteor.users.update({'profile.settings.quick_sell_options.unfound': null}, {$set: {'profile.settings.quick_sell_options.unfound': false}}, {multi: true});
     // temp code
 }
 
