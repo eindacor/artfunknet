6992a1a5d631baf1d902277a9869653984e3e5d1
Joseph Pollack <jpollack320@gmail.com>
2017-05-17 11:02:13 -0400

    hotfix missing settings bug

--- server/server.js
+++ server/server.js
@@ -55,7 +55,22 @@ var updateContent = function() {
     }
 
     // temp code
-    quests.update({}, {$set: {'target': quests.findOne().target}}, {multi: true});
+    var quick_sell_options = {
+	    'foil': false,
+	    'legendary': false,
+	    'masterpiece': false,
+	    'original': false,
+	    'quest_items': false,
+	    'seasonal': false,
+	    'standard': true,
+	    'sought': true,
+	    'unfound': false,
+	    'unlocked': false,
+	    'vintage': false
+	}
+    
+    Meteor.users.update({'profile.settings.quick_sell_options.standard': null}, {$set: {'profile.settigns.quick_sell_options': quick_sell_options}}, {multi: true});
+
     // temp code
 }
 
