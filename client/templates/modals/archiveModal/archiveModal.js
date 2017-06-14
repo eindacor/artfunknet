80269a305fbf5261a9eb36f7e65bb5b87380301a
Joseph Pollack <jpollack320@gmail.com>
2017-06-14 01:26:48 -0400

    fix archive modal bug

--- client/templates/modals/archiveModal/archiveModal.js
+++ client/templates/modals/archiveModal/archiveModal.js
@@ -26,7 +26,9 @@ Template.archiveModal.helpers({
 	'displaced_item': function(item_data) {
 		var player_item_interface = new PlayerItemIF(new PlayerIF(Meteor.user()), new ItemIF(item_data));
 		displaced_item = player_item_interface.getDisplacedArchiveItem();
-		displaced_item.displaced = true;
+		if (displaced_item) {
+			displaced_item.displaced = true;
+		}
 		return displaced_item;
 	}
 })
