c4fa1327fa629a25ca12dc8877e3d5f0da31f997
Joseph Pollack <jpollack320@gmail.com>
2017-06-29 20:25:12 -0400

    update experts to include repairing status

--- server/npc_interactions/artExpertInteraction.js
+++ server/npc_interactions/artExpertInteraction.js
@@ -29,14 +29,12 @@ artExpertInteraction = function(npc_object, player_interface) {
 			}
 		}
 
-		
-
 		if (player_interface.procUniqueAttribute("NEGATIVE_ROLL_COUNTS", undefined)) {
 			roll_count_min = -5;
 		}
 	}
 
-	var highest_item = getOneFromCollection("artExpertInteraction", items, {'owner' : Meteor.userId(), 'status' : {$in : ["claimed", "displayed"]}, 'roll_count' : {$gt : roll_count_min}}, {sort: {'roll_count': -1}});
+	var highest_item = getOneFromCollection("artExpertInteraction", items, {'owner' : Meteor.userId(), 'status' : {$in : ["claimed", "displayed", "repairing"]}, 'roll_count' : {$gt : roll_count_min}}, {sort: {'roll_count': -1}});
 
 	if (highest_item == undefined) {
 		var display_count = items.find({'owner': player_interface.getId(), 'status': "displayed"}).count();
