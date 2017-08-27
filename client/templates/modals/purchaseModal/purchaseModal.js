241e2c09910a803fcb36b9378b5142999caf3259
Joseph Pollack <jpollack320@gmail.com>
2017-08-26 20:21:45 -0400

    fix purchase modal bug

--- client/templates/modals/purchaseModal/purchaseModal.js
+++ client/templates/modals/purchaseModal/purchaseModal.js
@@ -1,7 +1,7 @@
 Template.purchaseModal.events({
 	'click #purchase-artwork' : function(element) {
 		var item_id = $(element.target).data().item_id;
-		Meteor.call('purchaseItemFromDealer', item_id, function(error) {
+		Meteor.call('purchaseItem', item_id, function(error) {
 			if (error)
 				console.log(error.message);
 
