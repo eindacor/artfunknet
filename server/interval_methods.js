eabb8507f1752622f4778dab9c1fdac2bf534785
Joseph Pollack <jpollack320@gmail.com>
2017-12-03 00:14:17 -0500

    fix typo

--- server/interval_methods.js
+++ server/interval_methods.js
@@ -10,7 +10,7 @@ Meteor.setInterval((function() {
         removeItem(item_object._id, "failed to claim (" + item_object.status + ")", undefined);
     })
 
-    var store_creation_cutoff = moment().add(-1 * 20 * ONE_MINUE, 'milliseconds')._d.toISOString();
+    var store_creation_cutoff = moment().add(-1 * 20 * ONE_MINUTE, 'milliseconds')._d.toISOString();
     getFromCollection("interval_methods.js remove unpurchased", items, {'status' : 'for_sale', 'date_received' : {$lt : store_creation_cutoff}}).forEach(function(item_object) {
         removeItem(item_object._id, "failed to claim (" + item_object.status + ")", undefined);
     })
