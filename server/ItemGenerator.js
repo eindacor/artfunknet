b7219c381e3b12e539f3b71d158fd5ce79d16171
Joseph Pollack <jpollack320@gmail.com>
2018-05-20 23:01:46 -0400

    fix item generator bug

--- server/ItemGenerator.js
+++ server/ItemGenerator.js
@@ -373,11 +373,13 @@ ItemGenerator = function() {
 	        'tags': [],
 	        'artwork_data': artwork_data,
 	        'permanent': false,
-	        'repairing': false,
-	        'odds': getItemOddsString(new_item_object),
-	        'values': getItemObjectValues(new_item_object),
-	        'reroll_cost': getItemObjectRollCost(new_item_object)
+	        'repairing': false
 	    };
+
+	    new_item_object.odds = getItemOddsString(new_item_object);
+
+	    new_item_object.values = getItemObjectValues(new_item_object);
+	    new_item_object.reroll_cost = getItemObjectRollCost(new_item_object);
 
 	    var new_item_id = insertItem(new_item_object, item_generator_object.source, callback);
 	    
