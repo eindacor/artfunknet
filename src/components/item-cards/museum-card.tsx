52bfc55fe04273d96d1a77ed911c24f9427d1b08
Joseph Pollack <jpollack320@gmail.com>
2026-09-03 11:00:17 -0400

    minor fixes to item cards

--- src/components/item-cards/museum-card.tsx
+++ src/components/item-cards/museum-card.tsx
@@ -12,12 +12,14 @@ export default function MuseumCard(props: ItemCardRendererProps) {
     <div className="render-card museum-card">
       <ArtworkImage className="museum-card-image" item={item} />
       <div className="museum-card-label">
-        <div className="museum-card-main">
+        <span className="museum-card-number card-rarity-label">
+          AF · {item.artwork.rarity}
+        </span>
+        <div className="museum-card-middle">
           <div className="museum-card-info">
-            <span className="museum-card-number card-rarity-label">
-              AF · {item.artwork.rarity}
-            </span>
-            <h3>{item.artwork.title}</h3>
+            <h3>
+              <span>{item.artwork.title}</span>
+            </h3>
             <p className="render-card-artist">{item.artwork.artist}</p>
             <p className="museum-card-workline">
               {item.artwork.date}, {item.artwork.medium}
